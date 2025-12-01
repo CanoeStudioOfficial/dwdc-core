3691653abf4256b9951fb7d31fe328f9826e66d3
Lonelyxiya <3438505487@qq.com>
2025-12-01 11:08:16 +0800

    updata

---
+++ src/main/java/com/canoestudio/dwdccore/DWDCcore.java
@@ -0,0 +1,33 @@
+package com.canoestudio.dwdccore;
+
+import com.canoestudio.dwdccore.proxy.CommonProxy;
+import net.minecraftforge.common.util.PacketUtil;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
+public class DWDCcore {
+
+    public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
+    public static final String MOD_ID = Tags.MOD_ID;
+
+    @SidedProxy(clientSide = "com.canoestudio.dwdccore.proxy.ClientProxy", serverSide = "com.canoestudio.dwdccore.proxy.CommonProxy")
+    public static CommonProxy proxy;
+
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        LOGGER.info("Hello From {}!", Tags.MOD_NAME);
+        proxy.preInit(event); }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) { proxy.init(event); }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) { proxy.postInit(event); }
+}
---
+++ src/main/java/com/canoestudio/dwdccore/compat/screenshotclipboard/ScreenshotToClipboard.java
@@ -0,0 +1,50 @@
+package com.canoestudio.dwdccore.compat.screenshotclipboard;
+
+
+import com.canoestudio.dwdccore.DWDCcore;
+import net.minecraftforge.client.event.ScreenshotEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+@Mod.EventBusSubscriber(modid = DWDCcore.MOD_ID)
+public class ScreenshotToClipboard {
+
+    @SubscribeEvent
+    public static void handleScreenshot(ScreenshotEvent event) {
+        new Thread(() -> {
+            Transferable trans = getTransferableImage(event.getImage());
+            Clipboard c = Toolkit.getDefaultToolkit().getSystemClipboard();
+            c.setContents(trans, null);
+        }).start();
+    }
+
+    private static Transferable getTransferableImage(final BufferedImage bufferedImage) {
+        return new Transferable() {
+            @Override
+            public DataFlavor[] getTransferDataFlavors() {
+                return new DataFlavor[] { DataFlavor.imageFlavor };
+            }
+
+            @Override
+            public boolean isDataFlavorSupported(DataFlavor flavor) {
+                return DataFlavor.imageFlavor.equals(flavor);
+            }
+
+            @Override
+            public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
+                if (DataFlavor.imageFlavor.equals(flavor)) {
+                    return bufferedImage;
+                }
+                throw new UnsupportedFlavorException(flavor);
+            }
+        };
+    }
+}
