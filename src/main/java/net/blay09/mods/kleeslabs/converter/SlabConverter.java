0ad87127bd8dae878b4d14fbd72aede54caf5add
Lonelyxiya <3438505487@qq.com>
2026-02-12 20:23:13 +0800

    1

---
+++ src/main/java/net/blay09/mods/kleeslabs/converter/SlabConverter.java
@@ -0,0 +1,14 @@
+package net.blay09.mods.kleeslabs.converter;
+
+import net.minecraft.block.BlockSlab;
+import net.minecraft.block.state.IBlockState;
+
+public interface SlabConverter {
+
+	IBlockState getSingleSlab(IBlockState state, BlockSlab.EnumBlockHalf blockHalf);
+
+	default boolean isDoubleSlab(IBlockState state) {
+		return true;
+	}
+
+}
