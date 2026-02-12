7ea6e2e96344876d74b47ed1fc9e7643ef6dfc4b
Lonelyxiya <3438505487@qq.com>
2026-02-12 23:09:42 +0800

    whatwhat

--- src/main/java/com/canoestudio/dwdccore/DWDCcore.java
+++ src/main/java/com/canoestudio/dwdccore/DWDCcore.java
@@ -1,7 +1,8 @@
 package com.canoestudio.dwdccore;
 
-import com.canoestudio.dwdccore.kleeslabs.KleeSlabs;
 import com.canoestudio.dwdccore.proxy.CommonProxy;
+
+import net.minecraftforge.common.util.PacketUtil;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -19,20 +20,15 @@ public class DWDCcore {
     @SidedProxy(clientSide = "com.canoestudio.dwdccore.proxy.ClientProxy", serverSide = "com.canoestudio.dwdccore.proxy.CommonProxy")
     public static CommonProxy proxy;
 
-    public static final KleeSlabs kleeslabs = new KleeSlabs();
-
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         LOGGER.info("Hello From {}!", Tags.MOD_NAME);
-        kleeslabs.preInit(event);
         proxy.preInit(event); }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) { proxy.init(event); }
 
     @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent event) {
-        kleeslabs.postInit(event);
-        proxy.postInit(event); }
+    public void postInit(FMLPostInitializationEvent event) { proxy.postInit(event); }
 }
